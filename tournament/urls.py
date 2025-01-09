0f91f18a6e0f012623f6f3d2b13f3e0aaa2e411e
wfarel <wfarel.t@gmail.com>
2025-01-09 18:03:30 -0400

    add teams views

--- tournament/urls.py
+++ tournament/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, teams
 
 urlpatterns = [
     path('', home, name='home'),
+    path('teams/', teams, name='teams'),
 ]
--- tournament/views.py
+++ tournament/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
-
+from .models import Team, Player
 # Create your views here.
 
 # home
 def home(request):
     return render(request, 'tournament/home.html')
+
+# teams
+def teams(request):
+    teams = Team.objects.all()
+    return render(request, 'tournament/teams.html', {'teams': teams})
